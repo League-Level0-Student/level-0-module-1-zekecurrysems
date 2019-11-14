7f677549417a020119bdcffa579e30cbcf42fbe7
curry.zeke <curry.zeke@SEMS-259-17.SEMS259.LAB>
2019-11-13 16:21:02 -0800

    Remarkable

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,6 +8,10 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
+String Lucas; 
+(Lucas.equals ("Lucas is a boomer"));
+		
+
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 
