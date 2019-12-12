7938633d1cef78c485178b9dc1b9ccc07fe35582
curry.zeke <curry.zeke@SEMS-259-17.SEMS259.LAB>
2019-12-11 16:25:28 -0800

    12/11/19

---
+++ src/_11_my_ages/my_ages.java
@@ -0,0 +1,49 @@
+package _11_my_ages;
+
+public class my_ages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+for (int i = 0; i < 13; i++) {
+System.out.println(i);	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
---
+++ src/_12_badgers/badgers.java
@@ -0,0 +1,68 @@
+package _12_badgers;
+
+import javax.swing.JOptionPane;
+
+public class badgers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+for (int i = 0; i < 10; i++) {
+JOptionPane.showMessageDialog(null, "Badger");	
+}	
+for (int i = 0; i < 2; i++) {
+JOptionPane.showMessageDialog(null, "Mushroom");
+}	
+for (int i = 0; i < 10; i++) {
+JOptionPane.showMessageDialog(null, "Badger");	
+}	
+for (int i = 0; i < 2; i++) {
+JOptionPane.showMessageDialog(null, "Mushroom");
+}	for (int i = 0; i < 10; i++) {
+	JOptionPane.showMessageDialog(null, "Badger");	
+	}	
+	
+	JOptionPane.showMessageDialog(null, "mush-Mushroom");
+	for (int i = 0; i < 10; i++) {
+		JOptionPane.showMessageDialog(null, "Badger");	
+		}	
+		
+		JOptionPane.showMessageDialog(null, "Argh! Snake, a snake!");
+JOptionPane.showMessageDialog(null, "Snaaake");					
+JOptionPane.showMessageDialog(null, "A snaaaake");
+JOptionPane.showMessageDialog(null, "oooh its a snake!");	
+JOptionPane.showMessageDialog(null, "It's a badger");	
+for (int i = 0; i < 10; i++) {
+JOptionPane.showMessageDialog(null, "It's a badger");	
+}	
+for (int i = 0; i < 2; i++) {
+JOptionPane.showMessageDialog(null, "Mushroom");
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+
+}
